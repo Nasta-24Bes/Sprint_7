9bcc1b84d3750065afa0b0514bffd94792a62ff9
Nasta-24Bes <nasta.bespalova@yandex.ru>
2025-12-17 10:21:55 +0300

    Update CourierClient.java

--- src/main/java/org/example/api/CourierClient.java
+++ src/main/java/org/example/api/CourierClient.java
@@ -1,5 +1,6 @@
 package org.example.api;
 
+import io.qameta.allure.Step;
 import io.restassured.response.Response;
 import org.example.Config;
 import org.example.dto.CourierCreateRequest;
@@ -7,14 +8,17 @@ import org.example.dto.CourierLoginRequest;
 
 public class CourierClient extends BaseClient {
 
+    @Step("Создание курьера")
     public static Response createCourier(CourierCreateRequest courier) {
         return post(Config.CREATE_COURIER, courier);
     }
 
+    @Step("Авторизация курьера")
     public static Response loginCourier(CourierLoginRequest loginData) {
         return post(Config.LOGIN_COURIER, loginData);
     }
 
+    @Step("Удаление курьера")
     public static Response deleteCourier(String id) {
         return delete(Config.DELETE_COURIER + id);
     }

7582a6d41f06866e6d9c8168ddd5b0e97264ed70
Nasta-24Bes <nasta.bespalova@yandex.ru>
2025-12-17 10:23:18 +0300

    Update CourierCreationTest.java

--- src/test/java/org/example/CourierCreationTest.java
+++ src/test/java/org/example/CourierCreationTest.java
@@ -20,12 +20,10 @@ public class CourierCreationTest extends BaseTest {
         Response createResponse = CourierClient.createCourier(courier);
         assertStatusCode(createResponse, SC_CREATED, "Создание курьера");
 
-        // Проверяем, что успешный запрос возвращает ok: true
         String responseBody = createResponse.getBody().asString();
         assertTrue("Успешный запрос должен возвращать ok: true",
                 responseBody.contains("\"ok\":true") || responseBody.contains("true"));
 
-        // Авторизуемся для получения ID
         CourierLoginRequest loginRequest = new CourierLoginRequest(courier.getLogin(), courier.getPassword());
         Response loginResponse = CourierClient.loginCourier(loginRequest);
 
@@ -38,22 +36,14 @@ public class CourierCreationTest extends BaseTest {
     public void testCreateDuplicateCourier() {
         CourierCreateRequest courier = createTestCourier();
 
-        // Первое создание
         Response firstCreate = CourierClient.createCourier(courier);
         assertStatusCode(firstCreate, SC_CREATED, "Первое создание курьера");
 
-        // Второе создание (дубликат) - ожидаем 409 CONFLICT
         Response secondCreate = CourierClient.createCourier(courier);
         assertStatusCode(secondCreate, SC_CONFLICT, "Создание дубликата курьера");
 
-        // Проверяем сообщение об ошибке
         String errorMessage = secondCreate.jsonPath().getString("message");
         assertNotNull("Должно быть сообщение об ошибке при дублировании", errorMessage);
-
-        // Сохраняем ID для очистки
-        CourierLoginRequest loginRequest = new CourierLoginRequest(courier.getLogin(), courier.getPassword());
-        Response loginResponse = CourierClient.loginCourier(loginRequest);
-        saveCourierId(loginResponse);
     }
 
     @Test
@@ -62,7 +52,6 @@ public class CourierCreationTest extends BaseTest {
         CourierCreateRequest courier = new CourierCreateRequest(null, "password123", "TestName");
 
         Response response = CourierClient.createCourier(courier);
-        // Согласно документации: 400 Bad Request
         assertStatusCode(response, SC_BAD_REQUEST, "Создание курьера без логина");
 
         String errorMessage = response.jsonPath().getString("message");
@@ -75,7 +64,6 @@ public class CourierCreationTest extends BaseTest {
         CourierCreateRequest courier = new CourierCreateRequest("testlogin", null, "TestName");
 
         Response response = CourierClient.createCourier(courier);
-        // Согласно документации: 400 Bad Request
         assertStatusCode(response, SC_BAD_REQUEST, "Создание курьера без пароля");
 
         String errorMessage = response.jsonPath().getString("message");
@@ -88,16 +76,7 @@ public class CourierCreationTest extends BaseTest {
         CourierCreateRequest courier = new CourierCreateRequest("testlogin", "password123", null);
 
         Response response = CourierClient.createCourier(courier);
-        // Согласно документации: firstName не обязательное поле
-        // Ожидаем либо 201 Created, либо 409 если такой логин уже есть
         int[] expectedStatuses = {SC_CREATED, SC_CONFLICT};
         assertStatusCodeFlexible(response, expectedStatuses, "Создание курьера без имени");
-
-        // Если создался успешно - сохраняем ID для очистки
-        if (response.statusCode() == SC_CREATED) {
-            CourierLoginRequest loginRequest = new CourierLoginRequest(courier.getLogin(), courier.getPassword());
-            Response loginResponse = CourierClient.loginCourier(loginRequest);
-            saveCourierId(loginResponse);
-        }
     }
 }

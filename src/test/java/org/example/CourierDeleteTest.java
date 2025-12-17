39b94f606865ffe9844bb49454c7befee94eb44a
Nasta-24Bes <nasta.bespalova@yandex.ru>
2025-12-17 10:23:39 +0300

    Update CourierDeleteTest.java

--- src/test/java/org/example/CourierDeleteTest.java
+++ src/test/java/org/example/CourierDeleteTest.java
@@ -5,6 +5,7 @@ import org.example.dto.CourierCreateRequest;
 import org.example.dto.CourierLoginRequest;
 import io.qameta.allure.junit4.DisplayName;
 import io.restassured.response.Response;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.apache.http.HttpStatus.*;
@@ -12,31 +13,38 @@ import static org.junit.Assert.*;
 
 public class CourierDeleteTest extends BaseTest {
 
-    @Test
-    @DisplayName("Успешное удаление курьера")
-    public void testDeleteCourierSuccess() {
+    private String testCourierId;
+
+    @Before
+    public void setUpTestCourier() {
         CourierCreateRequest courier = createTestCourier();
         Response createResponse = CourierClient.createCourier(courier);
         assertStatusCode(createResponse, SC_CREATED, "Создание курьера");
 
         CourierLoginRequest loginRequest = new CourierLoginRequest(courier.getLogin(), courier.getPassword());
         Response loginResponse = CourierClient.loginCourier(loginRequest);
-        String courierId = loginResponse.jsonPath().getString("id");
-        assertNotNull("ID курьера должен быть получен", courierId);
+        testCourierId = loginResponse.jsonPath().getString("id");
 
-        Response deleteResponse = CourierClient.deleteCourier(courierId);
+        createdCourierId = testCourierId;
+    }
+
+    @Test
+    @DisplayName("Успешное удаление курьера")
+    public void testDeleteCourierSuccess() {
+        Response deleteResponse = CourierClient.deleteCourier(testCourierId);
         assertStatusCode(deleteResponse, SC_OK, "Удаление курьера");
 
         String responseBody = deleteResponse.getBody().asString();
         assertTrue("Успешный запрос должен возвращать ok: true",
                 responseBody.contains("\"ok\":true") || responseBody.contains("true"));
+
+        createdCourierId = null;
     }
 
     @Test
     @DisplayName("Удаление курьера без ID")
     public void testDeleteCourierWithoutId() {
         Response response = CourierClient.deleteCourier("");
-        // Фактически API возвращает 404, а не 400
         assertStatusCode(response, SC_NOT_FOUND, "Удаление курьера без ID");
     }
 
@@ -45,7 +53,6 @@ public class CourierDeleteTest extends BaseTest {
     public void testDeleteCourierWithNonExistentId() {
         String nonExistentId = "999999";
         Response response = CourierClient.deleteCourier(nonExistentId);
-        // Фактически API возвращает 404, а не 400
         assertStatusCode(response, SC_NOT_FOUND, "Удаление курьера с несуществующим ID");
     }
 }
